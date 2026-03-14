33e3b72f9c99613f78febc1dc51b680ace4e83ac
Eduardo MS <eduardomorgado58@gmail.com>
2026-03-14 16:21:04 -0300

    Exercicio 04

---
+++ ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f;
+            Console.WriteLine("informe a temperatura em Celsius: ");
+            c = double.Parse(Console.ReadLine());
+            f = c * 9 / 5 + 32;
+            Console.WriteLine("em Fahrenheit esta: " + f);
+
+        }
+    }
+}
