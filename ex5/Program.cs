139af0c82a97e7c9544cefc2e5e6593ee10f6d5d
Eduardo MS <eduardomorgado58@gmail.com>
2026-03-14 16:29:26 -0300

    Exercicio 05

---
+++ ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.WriteLine("informe um numero: ");
+            n1 = double.Parse(Console.ReadLine());
+            n1 = n1 * 2;
+            Console.WriteLine("o doblo dele é: " + n1);
+        }
+    }
+}
