8786b8d4e1535df4f842a277c38cae7ed6d582a1
Eduardo MS <eduardomorgado58@gmail.com>
2026-03-14 15:48:41 -0300

    Exercicio 02

---
+++ ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2; 
+            Console.WriteLine("informe um numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("informe um outro numero: ");
+            n2 = int.Parse(Console.ReadLine());
+            n1 = n1 + n2;
+            Console.WriteLine("A soma deu: " + n1);
+        }
+    }
+}
