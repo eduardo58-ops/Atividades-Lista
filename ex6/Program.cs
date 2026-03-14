6d852fdb3bfb13243936aea43cd2f393427dfa24
Eduardo MS <eduardomorgado58@gmail.com>
2026-03-14 16:46:59 -0300

    Exercicio 06

---
+++ ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d, r;
+            Console.WriteLine("informe o valor: ");
+            d = double.Parse(Console.ReadLine());
+            r = d * 5.33; 
+            Console.WriteLine("o valor em real é: " + r);
+        }
+    }
+}
