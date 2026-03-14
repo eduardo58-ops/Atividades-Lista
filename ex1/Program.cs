bd55f2eb4c3e64d1e7dc6c6c934d79ecc7bf4e55
Eduardo MS <eduardomorgado58@gmail.com>
2026-03-14 15:06:28 -0300

    Exercicio 01

---
+++ ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu nome: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome);
+        }
+    }
+}
